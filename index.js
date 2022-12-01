395836475dd2b6f71814766eef81fb001613ba89
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2022-12-01 07:26:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function findMatching(driversname, match){
     })
     return filteredDriversname;
 }
-
+// * `fuzzyMatch` - This function takes an array of drivers' names and a `string`
+//   as arguments for querying the array, and returns all drivers whose names begin
+//   with the provided letters
+//split('') here it selects  drivers whose names have the specified letters
+//join('')does return an array as a string
+//.slice(o, matchLength) focuses of the a slice of a strings,extracts it and returns it as a new string
 function fuzzyMatch(drivers, match){
     const matchLength = match.split('').length;
     const filteredDrivers = drivers.filter((element) => {
@@ -20,6 +25,10 @@ function fuzzyMatch(drivers, match){
         }
     })
     return filteredDrivers;
+//     * `matchName` - This function takes an array of `driver` objects and a `string`
+//   as arguments. Each `driver` object has two properties: `name` and `hometown`.
+//   The function should return each element whose `name` property matches the
+//   provided `string` argument.
 }
 function matchName(drivers, match){
     const filteredDrivers = drivers.filter((element) => {
